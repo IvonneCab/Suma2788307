24be464d8487e8b6e04b7f6bc05ceeb842e58d41
JACKAGU <JACKAGU@DESKTOP-6D37A3T>
2024-08-19 11:46:15 -0600

    Agregar archivos de proyecto.

---
+++ Suma2788307/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Suma2788307
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Suma2788307/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Suma2788307
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
